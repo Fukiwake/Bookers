908e236d9f3026ec70608adb6d2bf053fdcb0e53
Fukiwake <tak0203753@gmail.com>
2021-01-06 05:16:21 +0000

    [Finish] 削除

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -19,7 +19,7 @@ class BooksController < ApplicationController
   def create
     book = Book.new(book_params)
     book.save
-    redirect_to book_path(b.id)
+    redirect_to book_path(book.id)
   end
 
   def edit
@@ -30,6 +30,12 @@ class BooksController < ApplicationController
     book = Book.find(params[:id])
     book.update(book_params)
     redirect_to book_path(book)
+  end
+  
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
   end
   
   private
